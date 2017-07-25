2d8ba84441bc4cccaca7e04c2424b0390863feae
JToeps <jim.toepel@gmail.com>
2017-07-24 21:18:30 -0700

    Initial Commit

---
+++ BusyTown/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BusyTown
+//
+//  Created by Jim Toepel on 7/24/17.
+//  Copyright © 2017 Jim Toepel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BusyTown/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BusyTown
+//
+//  Created by Jim Toepel on 7/24/17.
+//  Copyright © 2017 Jim Toepel. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
